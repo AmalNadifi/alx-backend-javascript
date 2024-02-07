2194d2c605ddcebb93332a678b51e1196c51854e
AmalNadifi <amal.nadifi9@gmail.com>
2024-02-07 13:19:55 +0000

    update solution to task2

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,10 +1,7 @@
 /* eslint-disable no-console */
-function handleResponseFromAPI(promise) {
-  return promise.then((response) => {
-    console.log("Got a response from the API");
-    return { status: 200, body: "success" };
-  }).catch((error) => {
-    console.error("Got an error from the API:", error);
-    return new Error();
-  });
+export default function handleResponseFromAPI(promise) {
+  return promise
+    .then(() => ({ status: 200, body: 'success' }))
+    .catch(() => Error())
+    .finally(() => { console.log('Got a response from the API'); });
 }
