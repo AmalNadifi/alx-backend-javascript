a44a2f1b23bb88d36721d1739738e8847427a2d0
AmalNadifi <amal.nadifi9@gmail.com>
2024-02-12 16:51:03 +0000

    update solution to task 4

--- 0x03-ES6_data_manipulation/4-update_grade_by_city.js
+++ 0x03-ES6_data_manipulation/4-update_grade_by_city.js
@@ -1,14 +1,22 @@
 /* eslint-disable no-param-reassign */
 /* eslint-disable no-prototype-builtins */
-export default function updateStudentGradeByCity(students, city, newGrades) {
-  return students
-    .filter(student => student.location === city)
-    .map(student => {
-      const matchingGrade = newGrades.find(grade => grade.studentId === student.id);
-      if (matchingGrade) {
-        return { ...student, grade: matchingGrade.grade };
-      } else {
-        return { ...student, grade: 'N/A' };
-      }
+export default function updateStudentGradeByCity(list, city, newGrade) {
+  return list
+    .filter((obj) => obj.location === city)
+    .map((student) => {
+    //   console.log(newGrade);
+      newGrade.map((studentGrade) => {
+        if (studentGrade.studentId === student.id) {
+          // eslint-disable-next-line no-param-reassign
+          student.grade = studentGrade.grade;
+        }
+
+        if (!student.hasOwnProperty('grade')) {
+          student.grade = 'N/A';
+        }
+        return student;
+      });
+
+      return student;
     });
 }
