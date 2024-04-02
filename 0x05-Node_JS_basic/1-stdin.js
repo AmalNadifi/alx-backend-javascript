e0650d496d46e46f8269bf1dea4177c746953aaa
AmalNadifi <amal.nadifi9@gmail.com>
2024-04-02 15:55:43 -0700

    Task1. Using Process stdin

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,16 +1,16 @@
-process.stdin.setEncoding('utf8');
-
 process.stdout.write('Welcome to Holberton School, what is your name?\n');
 
-process.stdin.on('data', (input) => {
-  const name = input.trim();
-
-  if (name) {
-    process.stdout.write(`Your name is: ${name}\n`);
+if (process.stdin.isTTY) {
+  process.stdin.on('data', (data) => {
+    process.stdout.write(`Your name is: ${data.toString()}`);
     process.exit();
-  }
-});
-
-process.on('exit', () => {
-  process.stdout.write('This important software is now closing\n');
-});
+  });
+} else {
+  process.stdin.on('data', (data) => {
+    process.stdout.write(`Your name is: ${data.toString()}`);
+    process.exit();
+  });
+  process.on('exit', () => {
+    process.stdout.write('This important software is now closing\n');
+  });
+}
