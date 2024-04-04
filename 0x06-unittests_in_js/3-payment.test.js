df92eb6f5f244d9c842cc26a963f962862e07813
AmalNadifi <amal.nadifi9@gmail.com>
2024-04-03 20:34:32 -0700

    Task3. Spies

---
+++ 0x06-unittests_in_js/3-payment.test.js
@@ -0,0 +1,16 @@
+const sinon = require('sinon');
+const chai = require('chai');
+const sendPaymentRequestToApi = require('./3-payment');
+const Utils = require('./utils');
+
+const { expect } = chai;
+
+describe('sendPaymentRequestToApi', () => {
+  it('should call Utils.calculateNumber with correct arguments', () => {
+    const calculateNumberSpy = sinon.spy(Utils, 'calculateNumber');
+    sendPaymentRequestToApi(100, 20);
+    expect(calculateNumberSpy.calledOnce).to.be.true;
+    expect(calculateNumberSpy.calledWithExactly('SUM', 100, 20)).to.be.true;
+    calculateNumberSpy.restore(); // Restore the spy after the test
+  });
+});
